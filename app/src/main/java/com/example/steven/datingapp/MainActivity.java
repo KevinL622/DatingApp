2950ef2db0581a26e6b423ca4be140bd42e9e141
stevenpan91 <stevenpan91@gmail.com>
2018-08-29 06:50:20 +0900

    experimenting with popup

--- app/src/main/java/com/example/steven/datingapp/MainActivity.java
+++ app/src/main/java/com/example/steven/datingapp/MainActivity.java
@@ -1,8 +1,17 @@
 package com.example.steven.datingapp;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +19,15 @@ public class MainActivity extends AppCompatActivity {
     static {
         System.loadLibrary("native-lib");
     }
+
+    PopupWindow popUp;
+    LinearLayout layout;
+    TextView tv;
+    LinearLayout.LayoutParams params;
+    LinearLayout mainLayout;
+    Button but;
+    boolean click = true;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +37,66 @@ public class MainActivity extends AppCompatActivity {
         // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
         tv.setText(stringFromJNI());
+
+
+
+        popUp = new PopupWindow(this);
+        layout = new LinearLayout(this);
+        mainLayout = new LinearLayout(this);
+        tv = new TextView(this);
+
+
+        params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+        layout.setOrientation(LinearLayout.VERTICAL);
+        tv.setText("Pineapple on Pizza is ____\n" +
+                "a) Delicious\n" +
+                "b) An abomination\n" +
+                "c) Never tried it\n" +
+                "d) Meh");
+        layout.addView(tv, params);
+        popUp.setContentView(layout);
+        // popUp.showAtLocation(layout, Gravity.BOTTOM, 10, 10);
+        //mainLayout.addView(but, params);
+        setContentView(mainLayout);
+
+        setRepeatingQuestions();
+
+    }
+
+    private void setRepeatingQuestions() {
+
+        final Handler handler = new Handler();
+        Timer timer = new Timer();
+
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(new Runnable() {
+                    public void run() {
+                        try {
+                           displayQuestions();
+                        } catch (Exception e) {
+                            // error, do something
+                        }
+                    }
+                });
+            }
+        };
+
+        timer.schedule(task, 0, 1000);  // interval of one minute
+
+    }
+
+    public void displayQuestions(){
+        if (click) {
+            popUp.showAtLocation(layout, Gravity.BOTTOM, 10, 10);
+            popUp.update(50, 50, 300, 80);
+            click = false;
+        } else {
+            popUp.dismiss();
+            click = true;
+        }
     }
 
     /**
